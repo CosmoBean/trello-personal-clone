102241d8f2298d8a2d9fed39e69156268ea75694
CosmoBean <freelance.ein@gmail.com>
2023-06-15 11:06:41 +0530

    adding summary section to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,6 +26,13 @@ function Header() {
 
     </div>
     </div>
+
+    <div className="flex items-center justify-center px-5 py-2 md:py-5">
+      <p className="flex items-center p-5 pr-5 text-sm font-light shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
+        <UserCircleIcon color="#0055D1" className="inline-block h-10 w-10 mr-1" />
+        Please wait, as we summarize your tasks for the day....
+      </p>
+    </div>
     </header>
   )
 }
