46b07f84f5aacfdae1ac784285329f37e79c9424
CosmoBean <freelance.ein@gmail.com>
2023-06-16 16:02:49 +0530

    feat: drag and drop using react-beautiful-dnd

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ export default function Home() {
   return (
     <main>
       <Header />
-      
-      <h1 className="flex justify-center p-5">trello-personal-clone</h1>
       <Board />
     </main>
   )
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,17 +1,39 @@
+import { databases } from '@/appwrite';
 import { getTodosGroupedByColumn } from '@/utils/getTodosGroupedByColumn';
 import { create } from 'zustand'
 
 export interface BoardState {
     board: Board;
     getBoard: ()=>void
+    setBoardState: (board:Board)=>void
+    updateTodoInDB: (todo:Todo, columnId: TypedColumns)=>void
+
 }
+
 export const useBoardStore = create<BoardState>((set) => ({
   board: {
     columns: new Map<TypedColumns, Column>()
   },
+
   getBoard: async ()=>{
     const board = await getTodosGroupedByColumn()
     set({board});
+  },
+
+  setBoardState: (board:Board)=>{
+    set({board});
+  },
+  
+  updateTodoInDB: async (todo:Todo, columnId: TypedColumns)=>{
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId,
+      }
+    );
   }
 }))
 
